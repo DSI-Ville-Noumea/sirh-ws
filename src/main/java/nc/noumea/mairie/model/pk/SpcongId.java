0104c3774b3ab65d515488f2cc9183c3a2507643
nicno85 <nicno85@ville-noumea.nc>
2012-11-08 04:58:06 +0000

    SIRHWS-44

--- src/main/java/nc/noumea/mairie/model/pk/SpcongId.java
+++ src/main/java/nc/noumea/mairie/model/pk/SpcongId.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import nc.noumea.mairie.model.bean.Sptyco;
 
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.json.RooJson;
@@ -17,9 +21,11 @@ public class SpcongId implements Serializable {
 	public SpcongId() {
 	}
 
-	public SpcongId(Integer nomatr, Integer dateDeb) {
+	public SpcongId(Integer nomatr, Integer dateDeb,Integer rang,Sptyco typConge) {
 		this.nomatr = nomatr;
 		this.dateDeb = dateDeb;
+		this.rang = rang;
+		this.typConge = typConge;
 	}
 
 	@Column(name = "NOMATR", insertable = false, updatable = false, columnDefinition = "numeric")
@@ -27,4 +33,11 @@ public class SpcongId implements Serializable {
 
 	@Column(name = "DATDEB", insertable = false, updatable = false, columnDefinition = "numeric")
 	private Integer dateDeb;
+
+	@Column(name = "RANG", insertable = false, updatable = false, columnDefinition = "numeric")
+	private Integer rang;
+
+	@OneToOne
+	@JoinColumn(name = "TYPE2", referencedColumnName = "TYPE2",insertable=false,updatable=false)
+	private Sptyco typConge;
 }
