e5ccf7d1d94a166211d8fd0b14cda8918bd554ca
rayni84 <rayni84@ville-noumea.nc>
2012-12-05 04:12:52 +0000

    ajout du web service d'arbre des services de mairie ajout d'un parametre optionnel dans l'appel du web service de l'équipe d'un agent afin de pouvoir filtrer par service (au moyen du sigle) refactor de la création de l'arbre des services pour le rendre thread-safe

--- src/main/java/nc/noumea/mairie/model/service/ISiservService.java
+++ src/main/java/nc/noumea/mairie/model/service/ISiservService.java
@@ -3,6 +3,7 @@ package nc.noumea.mairie.model.service;
 import java.util.List;
 
 import nc.noumea.mairie.model.bean.Siserv;
+import nc.noumea.mairie.tools.ServiceTreeNode;
 
 public interface ISiservService {
 
@@ -15,6 +16,10 @@ public interface ISiservService {
 	public Siserv getService(String servi);
 
 	public List<String> getListServiceAgent(Integer idAgent);
+	
+	public List<String> getListServiceAgent(Integer idAgent, String sigleServiceParent);
+	
+	public ServiceTreeNode getAgentServiceTree(Integer idAgent);
 
 	public List<Siserv> getListServiceActif();
 
