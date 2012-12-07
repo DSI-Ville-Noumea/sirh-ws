8719bd310f362389905f6b41be2b723af007e9c8
nicno85 <nicno85@ville-noumea.nc>
2012-12-07 00:43:57 +0000

    SIRHWS-61

--- src/main/java/nc/noumea/mairie/model/service/eae/EaeService.java
+++ src/main/java/nc/noumea/mairie/model/service/eae/EaeService.java
@@ -18,14 +18,14 @@ public class EaeService implements IEaeService {
 	@Override
 	public List<Integer> listIdEaeByCampagneAndAgent(Integer idCampagneEae, Integer idAgent, List<String> listeCodeService) {
 		String reqService = "";
-		if (listeCodeService != null) {
+		if (listeCodeService != null && listeCodeService.size() != 0) {
 			reqService = " union select e.ID_EAE from EAE e inner join EAE_FICHE_POSTE fp on e.ID_EAE = fp.ID_EAE inner join EAE_EVALUE evalue on e.ID_EAE = evalue.ID_EAE where e.ID_CAMPAGNE_EAE =:idCampagne and fp.CODE_SERVICE in (:listeCodeService) and evalue.ID_AGENT!=:idAgent ";
 		}
 		String sql = "select e.ID_EAE from EAE e where ID_CAMPAGNE_EAE =:idCampagne and ID_DELEGATAIRE =:idAgent union select e.ID_EAE from EAE e inner join EAE_EVALUATEUR ev on e.ID_EAE = ev.ID_EAE where e.ID_CAMPAGNE_EAE = :idCampagne and ev.ID_AGENT = :idAgent union select e.ID_EAE from EAE e inner join EAE_FICHE_POSTE fp on e.ID_EAE = fp.ID_EAE where e.ID_CAMPAGNE_EAE = :idCampagne and fp.ID_SHD = :idAgent ";
 		Query query = eaeEntityManager.createNativeQuery(sql + reqService);
 		query.setParameter("idCampagne", idCampagneEae);
 		query.setParameter("idAgent", idAgent);
-		if (listeCodeService != null)
+		if (listeCodeService != null && listeCodeService.size() != 0)
 			query.setParameter("listeCodeService", listeCodeService);
 		List<Integer> lIdEae = query.getResultList();
 		return lIdEae;
@@ -34,15 +34,15 @@ public class EaeService implements IEaeService {
 	@Override
 	public Integer compterlistIdEaeByCampagneAndAgent(Integer idCampagneEae, Integer idAgent, List<String> listService) {
 		String reqService = "";
-		if (listService != null) {
-			reqService = " union select e.ID_EAE from EAE e inner join EAE_FICHE_POSTE fp on e.ID_EAE = fp.ID_EAE where e.ID_CAMPAGNE_EAE =:idCampagne and fp.CODE_SERVICE in (:listeCodeService)";
+		if (listService != null && listService.size() != 0) {
+			reqService = " union select e.ID_EAE from EAE e inner join EAE_FICHE_POSTE fp on e.ID_EAE = fp.ID_EAE where e.ID_CAMPAGNE_EAE =:idCampagne and fp.CODE_SERVICE in (:listeCodeService)  and evalue.ID_AGENT!=:idAgent ";
 		}
 		String sql = "select count(e.id_eae) from EAE e where e.id_EAE in ( select e.ID_EAE from EAE e where ID_CAMPAGNE_EAE =:idCampagne and ID_DELEGATAIRE =:idAgent union select e.ID_EAE from EAE e inner join EAE_EVALUATEUR ev on e.ID_EAE = ev.ID_EAE where e.ID_CAMPAGNE_EAE = :idCampagne and ev.ID_AGENT = :idAgent union select e.ID_EAE from EAE e inner join EAE_FICHE_POSTE fp on e.ID_EAE = fp.ID_EAE where e.ID_CAMPAGNE_EAE = :idCampagne and fp.ID_SHD = :idAgent "
 				+ reqService + " )";
 		Query query = eaeEntityManager.createNativeQuery(sql);
 		query.setParameter("idCampagne", idCampagneEae);
 		query.setParameter("idAgent", idAgent);
-		if (listService != null)
+		if (listService != null && listService.size() != 0)
 			query.setParameter("listeCodeService", listService);
 
 		BigDecimal nbRes = (BigDecimal) query.getSingleResult();
