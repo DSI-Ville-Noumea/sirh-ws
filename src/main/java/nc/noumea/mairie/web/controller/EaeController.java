bc6154b040a3f16734594568aaf45a3f9b81f287
nicno85 <nicno85@ville-noumea.nc>
2013-02-12 03:14:57 +0000

    Mise à jour de la javadoc

--- src/main/java/nc/noumea/mairie/web/controller/EaeController.java
+++ src/main/java/nc/noumea/mairie/web/controller/EaeController.java
@@ -25,8 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import flexjson.JSONSerializer;
-
 @RooWebJson(jsonObject = Eae.class)
 @Controller
 @RequestMapping("/eaes")
