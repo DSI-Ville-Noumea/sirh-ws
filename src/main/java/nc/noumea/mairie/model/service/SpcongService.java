6e8070adfb836ec6228e4e5fc0e5799880c9a8bb
nicno85 <nicno85@ville-noumea.nc>
2013-02-13 05:08:42 +0000

    SIRHWS-72 et SIRHWS-73

--- src/main/java/nc/noumea/mairie/model/service/SpcongService.java
+++ src/main/java/nc/noumea/mairie/model/service/SpcongService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import nc.noumea.mairie.model.bean.Spcong;
 
@@ -25,14 +25,10 @@ public class SpcongService implements ISpcongService {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		String dateJourString = sdf.format(dateJour);
 		Integer annee = Integer.valueOf(dateJourString.substring(0, 4)) - 1;
-		String datFinMax = annee.toString()
-				+ dateJourString.substring(4, dateJourString.length());
+		String datFinMax = annee.toString() + dateJourString.substring(4, dateJourString.length());
 
-		Query query = sirhEntityManager
-				.createQuery(
-						"select spcong from Spcong spcong "
-								+ "where spcong.id.nomatr=:nomatr and spcong.dateFin>=:datFin order by spcong.id.dateDeb desc",
-						Spcong.class);
+		TypedQuery<Spcong> query = sirhEntityManager.createQuery("select spcong from Spcong spcong "
+				+ "where spcong.id.nomatr=:nomatr and spcong.dateFin>=:datFin order by spcong.id.dateDeb desc", Spcong.class);
 		query.setParameter("nomatr", nomatr.intValue());
 		query.setParameter("datFin", Integer.valueOf(datFinMax));
 		List<Spcong> lcong = query.getResultList();
@@ -47,13 +43,9 @@ public class SpcongService implements ISpcongService {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 		String dateJourString = sdf.format(dateJour);
 		Integer annee = Integer.valueOf(dateJourString.substring(0, 4)) - 1;
-		String datFinMax = annee.toString()
-				+ dateJourString.substring(4, dateJourString.length());
-		Query query = sirhEntityManager
-				.createQuery(
-						"select spcong from Spcong spcong "
-								+ "where spcong.id.nomatr=:nomatr and spcong.dateFin<:dateFin order by spcong.id.dateDeb desc",
-						Spcong.class);
+		String datFinMax = annee.toString() + dateJourString.substring(4, dateJourString.length());
+		TypedQuery<Spcong> query = sirhEntityManager.createQuery("select spcong from Spcong spcong "
+				+ "where spcong.id.nomatr=:nomatr and spcong.dateFin<:dateFin order by spcong.id.dateDeb desc", Spcong.class);
 		query.setParameter("nomatr", nomatr.intValue());
 		query.setParameter("dateFin", Integer.valueOf(datFinMax));
 		List<Spcong> lcong = query.getResultList();
