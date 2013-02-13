819ae2a74e5ca35be3bc02efe2ca2a0ff020449c
nicno85 <nicno85@ville-noumea.nc>
2013-02-13 05:12:34 +0000

    Correction sur TODO

--- src/main/java/nc/noumea/mairie/model/pk/SivietId.java
+++ src/main/java/nc/noumea/mairie/model/pk/SivietId.java
@@ -16,13 +16,11 @@ import org.springframework.roo.addon.tostring.RooToString;
 public class SivietId implements Serializable {
 	@Override
 	public int hashCode() {
-		// TODO Auto-generated method stub
 		return super.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
 		return super.equals(obj);
 	}
 
--- src/main/java/nc/noumea/mairie/model/pk/SpcongId.java
+++ src/main/java/nc/noumea/mairie/model/pk/SpcongId.java
@@ -20,13 +20,11 @@ import org.springframework.roo.addon.tostring.RooToString;
 public class SpcongId implements Serializable {
 	@Override
 	public int hashCode() {
-		// TODO Auto-generated method stub
 		return super.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
 		return super.equals(obj);
 	}
 
