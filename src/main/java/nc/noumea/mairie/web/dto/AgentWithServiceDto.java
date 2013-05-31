0beefa1122660d2beef0caafc6a525b22cc037ca
nicno85 <nicno85@ville-noumea.nc>
2013-05-31 00:08:13 +0000

    Relatif à #2304 : xhengement sur le Dto de l'agent et le Dto de l'agent avec Service

--- src/main/java/nc/noumea/mairie/web/dto/AgentWithServiceDto.java
+++ src/main/java/nc/noumea/mairie/web/dto/AgentWithServiceDto.java
@@ -2,46 +2,16 @@ package nc.noumea.mairie.web.dto;
 
 import nc.noumea.mairie.model.bean.Agent;
 
-public class AgentWithServiceDto {
-
-	private String nom;
-	private String prenom;
-	private Integer idAgent;
+public class AgentWithServiceDto extends AgentDto {
 	private String service;
 	private String codeService;
 
 	public AgentWithServiceDto() {
-
+		super();
 	}
 
 	public AgentWithServiceDto(Agent agent) {
-		nom = agent.getDisplayNom();
-		prenom = agent.getDisplayPrenom();
-		idAgent = agent.getIdAgent();
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public Integer getIdAgent() {
-		return idAgent;
-	}
-
-	public void setIdAgent(Integer idAgent) {
-		this.idAgent = idAgent;
+		super(agent);
 	}
 
 	public String getService() {
