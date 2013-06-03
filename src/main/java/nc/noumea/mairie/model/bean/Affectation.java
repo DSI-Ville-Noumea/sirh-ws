56fcfd4544c61a3981dc67466a24d4ac46936351
nicno85 <nicno85@ville-noumea.nc>
2013-06-03 00:28:53 +0000

    #2309 : WS qui liste les agents mairie en activité

--- src/main/java/nc/noumea/mairie/model/bean/Affectation.java
+++ src/main/java/nc/noumea/mairie/model/bean/Affectation.java
@@ -19,16 +19,18 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooToString
 @RooJson
 @RooJpaActiveRecord(persistenceUnit = "sirhPersistenceUnit", identifierType = Integer.class, identifierColumn = "ID_AFFECTATION", identifierField = "idAffectation", schema = "SIRH", table = "AFFECTATION", versionField = "")
-@NamedQuery(
-		name = "getCurrentAffectation",
-		query = "select a.fichePoste.idFichePoste from Affectation a where a.agent.idAgent = :idAgent and a.dateDebutAff <= :today and (a.dateFinAff = '01/01/0001' or a.dateFinAff is null or a.dateFinAff >= :today)"
-		)
+@NamedQuery(name = "getCurrentAffectation", query = "select a.fichePoste.idFichePoste from Affectation a where a.agent.idAgent = :idAgent and a.dateDebutAff <= :today and (a.dateFinAff = '01/01/0001' or a.dateFinAff is null or a.dateFinAff >= :today)")
 public class Affectation {
 
 	@NotNull
 	@OneToOne
-	@JoinColumn(name = "ID_AGENT", referencedColumnName = "ID_AGENT")
+	@JoinColumn(name = "ID_AGENT", referencedColumnName = "ID_AGENT", insertable = false, updatable = false)
 	private Agent agent;
+
+	@NotNull
+	@OneToOne
+	@JoinColumn(name = "ID_AGENT", referencedColumnName = "ID_AGENT", insertable = false, updatable = false)
+	private AgentRecherche agentrecherche;
 
 	@NotNull
 	@OneToOne
@@ -47,8 +49,8 @@ public class Affectation {
 	@Column(name = "TEMPS_TRAVAIL")
 	private String tempsTravail;
 
-	@OneToOne(optional=true)
+	@OneToOne(optional = true)
 	@JoinColumn(name = "ID_FICHE_POSTE_SECONDAIRE", referencedColumnName = "ID_FICHE_POSTE")
 	private FichePoste fichePosteSecondaire;
-	
+
 }
