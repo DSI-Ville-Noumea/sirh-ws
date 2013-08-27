12c12bb04076b5fba178e85ed9c3246f3d4efbba
nicno85 <nicno85@ville-noumea.nc>
2013-08-27 22:51:46 +0000

    #3832 : Document CAP pour avis SG : avis SHD à imprimer

--- src/main/java/nc/noumea/mairie/model/service/IReportingService.java
+++ src/main/java/nc/noumea/mairie/model/service/IReportingService.java
@@ -3,6 +3,6 @@ package nc.noumea.mairie.model.service;
 public interface IReportingService {
 
 	byte[] getFichePosteReportAsByteArray(int idFichePoste) throws Exception;
-	byte[] getTableauAvancementsReportAsByteArray(int idCap, int idCadreEmploi) throws Exception;
+	byte[] getTableauAvancementsReportAsByteArray(int idCap, int idCadreEmploi,boolean avisEAE) throws Exception;
 	byte[] getArretesReportAsByteArray(String csvIdAgents, boolean isChangementClasse, int year) throws Exception;
 }
