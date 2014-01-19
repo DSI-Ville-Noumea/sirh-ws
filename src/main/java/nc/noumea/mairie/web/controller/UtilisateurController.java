85e1a25982c00192c788160b802a82146c888fac
nicno85 <nicno85@ville-noumea.nc>
2014-01-19 22:48:17 +0000

    #5600 : RECUP - WS alimentation manuelle du compteur de récup --> liste utilisateurs SIRH --> avant SIRH-ABS-WS

---
+++ src/main/java/nc/noumea/mairie/web/controller/UtilisateurController.java
@@ -0,0 +1,66 @@
+package nc.noumea.mairie.web.controller;
+
+import nc.noumea.mairie.model.bean.Agent;
+import nc.noumea.mairie.model.service.IAgentService;
+import nc.noumea.mairie.web.dto.ReturnMessageDto;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import flexjson.JSONSerializer;
+
+@RooWebJson(jsonObject = Agent.class)
+@Controller
+@RequestMapping("/utilisateur")
+public class UtilisateurController {
+
+	private Logger logger = LoggerFactory.getLogger(UtilisateurController.class);
+
+	@Autowired
+	private IAgentService agentSrv;
+
+	private String remanieIdAgent(Integer idAgent) {
+		String newIdAgent;
+		if (idAgent.toString().length() == 6) {
+			// on remanie l'idAgent
+			String matr = idAgent.toString().substring(2, idAgent.toString().length());
+			String prefixe = idAgent.toString().substring(0, 2);
+			newIdAgent = prefixe + "0" + matr;
+		} else {
+			newIdAgent = idAgent.toString();
+		}
+		return newIdAgent;
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/isUtilisateurSIRH", produces = "application/json;charset=utf-8", method = RequestMethod.GET)
+	@Transactional(readOnly = true)
+	public ResponseEntity<String> isUtilisateurSIRH(@RequestParam(value = "idAgent", required = true) Integer idAgent) {
+
+		logger.debug("entered GET [utilisateur/isUtilisateurSIRH] => isUtilisateurSIRH with parameters idAgent = {}",
+				idAgent);
+
+		// on remanie l'idAgent
+		String newIdAgent = remanieIdAgent(idAgent);
+
+		ReturnMessageDto srm = agentSrv.isUtilisateurSIRH(newIdAgent);
+
+		String response = new JSONSerializer().exclude("*.class").deepSerialize(srm);
+
+		if (!srm.getErrors().isEmpty()) {
+			return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+		} else {
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		}
+	}
+}
