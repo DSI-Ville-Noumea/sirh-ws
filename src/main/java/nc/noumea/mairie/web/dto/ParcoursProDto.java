a09bf688afc236e679db88e9288a954cbcffb753
nicno85 <nicno85@ville-noumea.nc>
2014-03-14 04:46:09 +0000

    Ajout de tests unitaires

--- src/main/java/nc/noumea/mairie/web/dto/ParcoursProDto.java
+++ src/main/java/nc/noumea/mairie/web/dto/ParcoursProDto.java
@@ -9,12 +9,12 @@ import nc.noumea.mairie.model.bean.Spmtsr;
 public class ParcoursProDto {
 
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-	
+
 	private Date dateDebut;
 	private Date dateFin;
 	private String service;
 	private String direction;
-	
+
 	public ParcoursProDto(Spmtsr spMtsr) {
 		try {
 			this.dateDebut = sdf.parse(spMtsr.getDatdeb().toString());
@@ -22,36 +22,42 @@ public class ParcoursProDto {
 			this.dateDebut = null;
 		}
 		try {
-			this.dateFin = sdf.parse(spMtsr.getDatfin().toString());
+			this.dateFin = spMtsr.getDatfin() == null ? null : sdf.parse(spMtsr.getDatfin().toString());
 		} catch (ParseException e) {
-			this.dateDebut = null;
+			this.dateFin = null;
 		}
 	}
-	
+
 	public Date getDateDebut() {
 		return dateDebut;
 	}
+
 	public void setDateDebut(Date dateDebut) {
 		this.dateDebut = dateDebut;
 	}
+
 	public Date getDateFin() {
 		return dateFin;
 	}
+
 	public void setDateFin(Date dateFin) {
 		this.dateFin = dateFin;
 	}
+
 	public String getService() {
 		return service;
 	}
+
 	public void setService(String service) {
 		this.service = service;
 	}
+
 	public String getDirection() {
 		return direction;
 	}
+
 	public void setDirection(String direction) {
 		this.direction = direction;
 	}
-	
-	
+
 }
