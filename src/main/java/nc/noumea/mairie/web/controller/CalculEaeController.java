3f6de372ee90c23302e40624a5294e48a5583dca
nicno85 <nicno85@ville-noumea.nc>
2014-03-17 21:01:57 +0000

    Ajout d'informations dans les logs

--- src/main/java/nc/noumea/mairie/web/controller/CalculEaeController.java
+++ src/main/java/nc/noumea/mairie/web/controller/CalculEaeController.java
@@ -32,139 +32,157 @@ import flexjson.JSONSerializer;
 public class CalculEaeController {
 
 	private Logger logger = LoggerFactory.getLogger(CalculEaeController.class);
-	
+
 	@Autowired
 	private ICalculEaeService calculEaeService;
-	
+
 	@Autowired
 	private IAvancementsService avancementsService;
-	
+
 	@Autowired
 	private ISpCarrService spCarrService;
-	
+
 	@Autowired
 	private ISpadmnService spadmnService;
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/listeAgentEligibleEAESansAffectes", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
 	public ResponseEntity<String> getListeAgentEligibleEAESansAffectes() {
-		
+
 		logger.debug("entered GET [calculEae/listeAgentEligibleEAESansAffectes] => getListeAgentEligibleEAESansAffectes ");
-		
+
 		List<AgentDto> result = calculEaeService.getListeAgentEligibleEAESansAffectes();
-		
+
 		logger.debug("FIN [calculEae/listeAgentEligibleEAESansAffectes] : " + result.size());
 
 		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").serialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/listeAgentEligibleEAEAffectes", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
 	public ResponseEntity<String> getListeAgentEligibleEAEAffectes() {
-		
+
 		logger.debug("entered GET [calculEae/listeAgentEligibleEAEAffectes] => getListeAgentEligibleEAEAffectes ");
-		
+
 		List<AgentDto> result = calculEaeService.getListeAgentEligibleEAEAffectes();
-		
+
 		logger.debug("FIN [calculEae/listeAgentEligibleEAEAffectes] : " + result.size());
 
 		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").serialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/avancement", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getAvancement (
-			@RequestParam("idAgent") int idAgent, @RequestParam("anneeAvancement") int anneeAvancement, @RequestParam("isFonctionnaire") boolean isFonctionnaire) {
-		
-		logger.debug("entered GET [calculEae/avancement] => getAvancement");
-		
+	public ResponseEntity<String> getAvancement(@RequestParam("idAgent") int idAgent,
+			@RequestParam("anneeAvancement") int anneeAvancement,
+			@RequestParam("isFonctionnaire") boolean isFonctionnaire) {
+
+		logger.debug(
+				"entered GET [calculEae/avancement] => getAvancement with parameter idAgent = {}, anneeAvancement = {}, isFonctionnaire = {}",
+				idAgent, anneeAvancement, isFonctionnaire);
+
 		AvancementEaeDto result = avancementsService.getAvancement(idAgent, anneeAvancement, isFonctionnaire);
 
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class).serialize(result), HttpStatus.OK);
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).serialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/avancementDetache", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getAvancementDetache (
-			@RequestParam("idAgent") int idAgent, @RequestParam("anneeAvancement") int anneeAvancement) {
-		
-		logger.debug("entered GET [calculEae/avancementDetache] => getAvancementDetache");
-		
+	public ResponseEntity<String> getAvancementDetache(@RequestParam("idAgent") int idAgent,
+			@RequestParam("anneeAvancement") int anneeAvancement) {
+
+		logger.debug(
+				"entered GET [calculEae/avancementDetache] => getAvancementDetache with parameter idAgent = {}, anneeAvancement = {}",
+				idAgent, anneeAvancement);
+
 		AvancementEaeDto result = avancementsService.getAvancementDetache(idAgent, anneeAvancement);
 
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class).serialize(result), HttpStatus.OK);
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).serialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/affectationActiveByAgent", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getDetailAffectationActiveByAgent(@RequestParam("idAgent") int idAgent, @RequestParam("anneeFormation") int anneeFormation) {
-		
-		logger.debug("entered GET [calculEae/affectationActiveByAgent] => getAffectationActiveByAgent ");
-		
+	public ResponseEntity<String> getDetailAffectationActiveByAgent(@RequestParam("idAgent") int idAgent,
+			@RequestParam("anneeFormation") int anneeFormation) {
+
+		logger.debug(
+				"entered GET [calculEae/affectationActiveByAgent] => getAffectationActiveByAgent with parameter idAgent = {}, anneeFormation = {}",
+				idAgent, anneeFormation);
+
 		CalculEaeInfosDto result = calculEaeService.getAffectationActiveByAgent(idAgent, anneeFormation);
 
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result), HttpStatus.OK);
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).deepSerialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/listeAffectationsAgentAvecService", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getListeAffectationsAgentAvecService(
-			@RequestParam("idAgent") int idAgent,
+	public ResponseEntity<String> getListeAffectationsAgentAvecService(@RequestParam("idAgent") int idAgent,
 			@RequestParam("idService") String idService) {
-		
-		logger.debug("entered GET [calculEae/listeAffectationsAgentAvecService] => getListeAffectationsAgentAvecService ");
-		
+
+		logger.debug(
+				"entered GET [calculEae/listeAffectationsAgentAvecService] => getListeAffectationsAgentAvecService with parameter idAgent = {}, idService = {}",
+				idAgent, idService);
+
 		List<CalculEaeInfosDto> result = calculEaeService.getListeAffectationsAgentAvecService(idAgent, idService);
 
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result), HttpStatus.OK);
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).deepSerialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/listeAffectationsAgentAvecFP", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> getListeAffectationsAgentAvecFP(
-			@RequestParam("idAgent") int idAgent,
+	public ResponseEntity<String> getListeAffectationsAgentAvecFP(@RequestParam("idAgent") int idAgent,
 			@RequestParam("idFichePoste") int idFichePoste) {
-		
-		logger.debug("entered GET [calculEae/listeAffectationsAgentAvecFP] => getListeAffectationsAgentAvecFP ");
-		
+
+		logger.debug(
+				"entered GET [calculEae/listeAffectationsAgentAvecFP] => getListeAffectationsAgentAvecFP  with parameter idAgent = {}, idFichePoste = {}",
+				idAgent, idFichePoste);
+
 		List<CalculEaeInfosDto> result = calculEaeService.getListeAffectationsAgentAvecFP(idAgent, idFichePoste);
 
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result), HttpStatus.OK);
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).deepSerialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/autreAdministrationAgentAncienne", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ResponseEntity<String> chercherAutreAdministrationAgentAncienne(@RequestParam("idAgent") int idAgent, @RequestParam("isFonctionnaire") boolean isFonctionnaire) {
-		
-		logger.debug("entered GET [calculEae/autreAdministrationAgentAncienne] => chercherAutreAdministrationAgentAncienne ");
-		
-		AutreAdministrationAgentDto result = calculEaeService.chercherAutreAdministrationAgentAncienne(idAgent, isFonctionnaire);
-		
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result), HttpStatus.OK);
+	public ResponseEntity<String> chercherAutreAdministrationAgentAncienne(@RequestParam("idAgent") int idAgent,
+			@RequestParam("isFonctionnaire") boolean isFonctionnaire) {
+
+		logger.debug(
+				"entered GET [calculEae/autreAdministrationAgentAncienne] => chercherAutreAdministrationAgentAncienne with parameter idAgent = {}, isFonctionnaire = {}",
+				idAgent, isFonctionnaire);
+
+		AutreAdministrationAgentDto result = calculEaeService.chercherAutreAdministrationAgentAncienne(idAgent,
+				isFonctionnaire);
+
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).deepSerialize(result), HttpStatus.OK);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/listeAutreAdministrationAgent", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
 	public ResponseEntity<String> getListeAutreAdministrationAgent(@RequestParam("idAgent") int idAgent) {
-		
-		logger.debug("entered GET [calculEae/listeAutreAdministrationAgent] => getListeAutreAdministrationAgent ");
-		
+
+		logger.debug(
+				"entered GET [calculEae/listeAutreAdministrationAgent] => getListeAutreAdministrationAgent with parameter idAgent = {}",
+				idAgent);
+
 		List<AutreAdministrationAgentDto> result = calculEaeService.getListeAutreAdministrationAgent(idAgent);
-		
-		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
-				.deepSerialize(result), HttpStatus.OK);
+
+		return new ResponseEntity<String>(new JSONSerializer().exclude("*.class")
+				.transform(new MSDateTransformer(), Date.class).deepSerialize(result), HttpStatus.OK);
 	}
 }
