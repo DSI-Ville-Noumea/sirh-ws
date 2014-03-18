b4e381ff2e0a574fce85fb448928f81396cb94de
nicno85 <nicno85@ville-noumea.nc>
2014-03-18 03:28:40 +0000

    Ajout de tests unitaires

--- src/test/java/nc/noumea/mairie/service/sirh/AgentMatriculeConverterServiceTest.java
+++ src/test/java/nc/noumea/mairie/service/sirh/AgentMatriculeConverterServiceTest.java
@@ -1,7 +1,6 @@
 package nc.noumea.mairie.service.sirh;
 
 import static org.junit.Assert.assertEquals;
-import nc.noumea.mairie.service.sirh.AgentMatriculeConverterService;
 
 import org.junit.Test;
 
@@ -30,6 +29,34 @@ public class AgentMatriculeConverterServiceTest {
 
 		// When
 		int result = service.fromADIdAgentToEAEIdAgent(theIdToConvert);
+
+		// Then
+		assertEquals(9006898, result);
+	}
+
+	@Test
+	public void testTryConvertFromADIdAgentToEAEIdAgent_withIdIs5digits_convertItTo6Digits() {
+
+		// Given
+		int theIdToConvert = 906898;
+		AgentMatriculeConverterService service = new AgentMatriculeConverterService();
+
+		// When
+		int result = service.tryConvertFromADIdAgentToEAEIdAgent(theIdToConvert);
+
+		// Then
+		assertEquals(9006898, result);
+	}
+
+	@Test
+	public void testTryConvertFromADIdAgentToEAEIdAgent_withIdIs6digits() {
+
+		// Given
+		int theIdToConvert = 9006898;
+		AgentMatriculeConverterService service = new AgentMatriculeConverterService();
+
+		// When
+		int result = service.tryConvertFromADIdAgentToEAEIdAgent(theIdToConvert);
 
 		// Then
 		assertEquals(9006898, result);
