e1cb2ff478bfaa96c4b88f06982458aa7bb61db4
nicno85 <nicno85@ville-noumea.nc>
2014-04-29 22:51:59 +0000

    #7429 : Supprimer SIIDMA et utiliser RADI --> CONTEXT

--- src/test/java/nc/noumea/mairie/service/sirh/UtilisateurServiceTest.java
+++ src/test/java/nc/noumea/mairie/service/sirh/UtilisateurServiceTest.java
@@ -9,13 +9,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import nc.noumea.mairie.model.bean.Agent;
-import nc.noumea.mairie.model.bean.Siidma;
 import nc.noumea.mairie.model.bean.Utilisateur;
-import nc.noumea.mairie.model.pk.SiidmaId;
-import nc.noumea.mairie.service.SiidmaService;
-import nc.noumea.mairie.service.sirh.AgentService;
-import nc.noumea.mairie.service.sirh.UtilisateurService;
+import nc.noumea.mairie.web.dto.LightUserDto;
 import nc.noumea.mairie.web.dto.ReturnMessageDto;
+import nc.noumea.mairie.ws.RadiWSConsumer;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -76,19 +73,20 @@ public class UtilisateurServiceTest {
 		AgentService agSrvEMMock = Mockito.mock(AgentService.class);
 		Mockito.when(agSrvEMMock.getAgent(9005138)).thenReturn(ag);
 
-		SiidmaService siidmaEMMock = Mockito.mock(SiidmaService.class);
-		Mockito.when(siidmaEMMock.chercherSiidmaByIdIndi(9005138)).thenReturn(null);
+		RadiWSConsumer radiMock = Mockito.mock(RadiWSConsumer.class);
+		Mockito.when(radiMock.getNomatrWithIdAgent(9005138)).thenReturn("5138");
+		Mockito.when(radiMock.getAgentCompteAD(5138)).thenReturn(null);
 
 		UtilisateurService siservService = new UtilisateurService();
 		ReflectionTestUtils.setField(siservService, "agentSrv", agSrvEMMock);
-		ReflectionTestUtils.setField(siservService, "siidmaSrv", siidmaEMMock);
+		ReflectionTestUtils.setField(siservService, "radiWSConsumer", radiMock);
 
 		// When
 		ReturnMessageDto result = siservService.isUtilisateurSIRH("9005138");
 
 		// Then
 		assertEquals(1, result.getErrors().size());
-		assertEquals("L'agent 9005138 n'existe pas dans SIIDMA.", result.getErrors().get(0));
+		assertEquals("L'agent 9005138 n'a pas de compte AD ou n'a pas son login renseigné.", result.getErrors().get(0));
 
 	}
 
@@ -97,20 +95,19 @@ public class UtilisateurServiceTest {
 		// Given
 		Agent ag = new Agent();
 		ag.setIdAgent(9005138);
-		SiidmaId id = new SiidmaId();
-		id.setCdidut("LOGIN");
-		id.setIdIndi(905138);
-		Siidma sii = new Siidma();
-		sii.setId(id);
-		sii.setNomatr(5138);
+
+		LightUserDto user = new LightUserDto();
+		user.setEmployeeNumber(905138);
+		user.setsAMAccountName("LOGIN");
 
 		List<Utilisateur> listUtil = new ArrayList<>();
 
 		AgentService agSrvEMMock = Mockito.mock(AgentService.class);
 		Mockito.when(agSrvEMMock.getAgent(9005138)).thenReturn(ag);
 
-		SiidmaService siidmaEMMock = Mockito.mock(SiidmaService.class);
-		Mockito.when(siidmaEMMock.chercherSiidmaByIdIndi(905138)).thenReturn(sii);
+		RadiWSConsumer radiMock = Mockito.mock(RadiWSConsumer.class);
+		Mockito.when(radiMock.getNomatrWithIdAgent(9005138)).thenReturn("5138");
+		Mockito.when(radiMock.getAgentCompteAD(5138)).thenReturn(user);
 
 		@SuppressWarnings("unchecked")
 		TypedQuery<Utilisateur> mockQuery = Mockito.mock(TypedQuery.class);
@@ -121,8 +118,8 @@ public class UtilisateurServiceTest {
 
 		UtilisateurService siservService = new UtilisateurService();
 		ReflectionTestUtils.setField(siservService, "agentSrv", agSrvEMMock);
-		ReflectionTestUtils.setField(siservService, "siidmaSrv", siidmaEMMock);
 		ReflectionTestUtils.setField(siservService, "sirhEntityManager", sirhEMMock);
+		ReflectionTestUtils.setField(siservService, "radiWSConsumer", radiMock);
 
 		// When
 		ReturnMessageDto result = siservService.isUtilisateurSIRH("9005138");
@@ -138,12 +135,10 @@ public class UtilisateurServiceTest {
 		// Given
 		Agent ag = new Agent();
 		ag.setIdAgent(9005138);
-		SiidmaId id = new SiidmaId();
-		id.setCdidut("LOGIN");
-		id.setIdIndi(905138);
-		Siidma sii = new Siidma();
-		sii.setId(id);
-		sii.setNomatr(5138);
+
+		LightUserDto user = new LightUserDto();
+		user.setEmployeeNumber(905138);
+		user.setsAMAccountName("LOGIN");
 
 		Utilisateur u = new Utilisateur();
 		u.setLogin("login");
@@ -153,9 +148,6 @@ public class UtilisateurServiceTest {
 		AgentService agSrvEMMock = Mockito.mock(AgentService.class);
 		Mockito.when(agSrvEMMock.getAgent(9005138)).thenReturn(ag);
 
-		SiidmaService siidmaEMMock = Mockito.mock(SiidmaService.class);
-		Mockito.when(siidmaEMMock.chercherSiidmaByIdIndi(905138)).thenReturn(sii);
-
 		@SuppressWarnings("unchecked")
 		TypedQuery<Utilisateur> mockQuery = Mockito.mock(TypedQuery.class);
 		Mockito.when(mockQuery.getResultList()).thenReturn(listUtil);
@@ -163,10 +155,14 @@ public class UtilisateurServiceTest {
 		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
 		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(Utilisateur.class))).thenReturn(mockQuery);
 
+		RadiWSConsumer radiMock = Mockito.mock(RadiWSConsumer.class);
+		Mockito.when(radiMock.getNomatrWithIdAgent(9005138)).thenReturn("5138");
+		Mockito.when(radiMock.getAgentCompteAD(5138)).thenReturn(user);
+
 		UtilisateurService siservService = new UtilisateurService();
 		ReflectionTestUtils.setField(siservService, "agentSrv", agSrvEMMock);
-		ReflectionTestUtils.setField(siservService, "siidmaSrv", siidmaEMMock);
 		ReflectionTestUtils.setField(siservService, "sirhEntityManager", sirhEMMock);
+		ReflectionTestUtils.setField(siservService, "radiWSConsumer", radiMock);
 
 		// When
 		ReturnMessageDto result = siservService.isUtilisateurSIRH("9005138");
