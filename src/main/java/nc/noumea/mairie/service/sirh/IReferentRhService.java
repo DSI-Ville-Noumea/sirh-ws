c2e1876cb4ea7a46441cf5ff49d2075e21c98bb6
nicno85 <nicno85@ville-noumea.nc>
2014-10-06 03:48:49 +0000

    #11341 : Créer les WS utiles aux Rérérents RH

---
+++ src/main/java/nc/noumea/mairie/service/sirh/IReferentRhService.java
@@ -0,0 +1,11 @@
+package nc.noumea.mairie.service.sirh;
+
+import java.util.List;
+
+import nc.noumea.mairie.model.bean.sirh.ReferentRh;
+
+public interface IReferentRhService {
+
+	List<ReferentRh> getListeReferentRH();
+
+}
