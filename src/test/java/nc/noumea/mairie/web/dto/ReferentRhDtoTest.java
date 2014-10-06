445653ed32f6d69b1fc49c517446c8b7a63bdf3f
nicno85 <nicno85@ville-noumea.nc>
2014-10-06 22:50:04 +0000

    #11466 : Test unitaires

---
+++ src/test/java/nc/noumea/mairie/web/dto/ReferentRhDtoTest.java
@@ -0,0 +1,53 @@
+package nc.noumea.mairie.web.dto;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import nc.noumea.mairie.model.bean.Siserv;
+import nc.noumea.mairie.model.bean.sirh.Agent;
+import nc.noumea.mairie.model.bean.sirh.ReferentRh;
+
+import org.junit.Test;
+
+public class ReferentRhDtoTest {
+
+	@Test
+	public void testReferentRhDto_cst() {
+		// Given
+		ReferentRh c = new ReferentRh();
+		c.setIdAgentReferent(21);
+		c.setNumeroTelephone(22);
+		c.setServi("toto");
+		Agent ag = new Agent();
+		ag.setPrenomUsage("prenom usage");
+		Siserv serv = new Siserv();
+		serv.setSigle("sigle");
+
+		// When
+		ReferentRhDto dto = new ReferentRhDto(c, ag, serv);
+
+		// Then
+		assertEquals(c.getIdAgentReferent(), dto.getIdAgentReferent());
+		assertEquals(c.getNumeroTelephone(), dto.getNumeroTelephone());
+		assertEquals(ag.getPrenomUsage(), dto.getPrenomAgentReferent());
+		assertEquals(c.getServi(), dto.getServi());
+		assertEquals(serv.getSigle(), dto.getSigleService());
+	}
+
+	@Test
+	public void testReferentRhDto_cstNull() {
+		// Given
+		ReferentRh c = null;
+		Agent ag = null;
+		Siserv serv = null;
+
+		// When
+		ReferentRhDto dto = new ReferentRhDto(c, ag, serv);
+
+		// Then
+		assertNull(dto.getIdAgentReferent());
+		assertNull(dto.getNumeroTelephone());
+		assertNull(dto.getPrenomAgentReferent());
+		assertNull(dto.getServi());
+		assertNull(dto.getSigleService());
+	}
+}
