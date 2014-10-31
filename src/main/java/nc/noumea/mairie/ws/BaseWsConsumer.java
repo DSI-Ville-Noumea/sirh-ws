122b596a63df8713ceedf472498b832d257a2442
rebjo84 <rebjo84@ville-noumea.nc>
2014-10-31 02:29:48 +0000

    correction WS Type Json

--- src/main/java/nc/noumea/mairie/ws/BaseWsConsumer.java
+++ src/main/java/nc/noumea/mairie/ws/BaseWsConsumer.java
@@ -38,12 +38,12 @@ public abstract class BaseWsConsumer {
 		try {
 			if (isPost)
 				if (postContent == null)
-					response = webResource.accept(MediaType.APPLICATION_JSON_VALUE).post(ClientResponse.class);
+					response = webResource.type(MediaType.APPLICATION_JSON_VALUE).post(ClientResponse.class);
 				else
-					response = webResource.accept(MediaType.APPLICATION_JSON_VALUE).post(ClientResponse.class,
+					response = webResource.type(MediaType.APPLICATION_JSON_VALUE).post(ClientResponse.class,
 							postContent);
 			else
-				response = webResource.accept(MediaType.APPLICATION_JSON_VALUE).get(ClientResponse.class);
+				response = webResource.type(MediaType.APPLICATION_JSON_VALUE).get(ClientResponse.class);
 		} catch (ClientHandlerException ex) {
 			throw new WSConsumerException(String.format("An error occured when querying '%s'.", url), ex);
 		}
