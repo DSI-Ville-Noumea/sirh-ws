ba10ce50581fba23f05b149533e0a26cb5008a46
nicno85 <nicno85@ville-noumea.nc>
2014-11-13 04:30:04 +0000

    #11956 : EAE --> erreur sur affectation active pour certains agents

--- src/main/java/nc/noumea/mairie/service/SpCarrService.java
+++ src/main/java/nc/noumea/mairie/service/SpCarrService.java
@@ -1,6 +1,7 @@
 package nc.noumea.mairie.service;
 
 import nc.noumea.mairie.model.bean.Spcarr;
+import nc.noumea.mairie.model.bean.SpcarrWithoutSpgradn;
 import nc.noumea.mairie.model.repository.ISpcarrRepository;
 import nc.noumea.mairie.web.dto.CarriereDto;
 
@@ -29,10 +30,19 @@ public class SpCarrService implements ISpCarrService {
 	public CarriereDto getCarriereActive(Integer noMatr) {
 
 		CarriereDto dto = null;
-		Spcarr carr = spcarrRepository.getCarriereActive(noMatr);
+		try {
+			Spcarr carr = spcarrRepository.getCarriereActive(noMatr);
 
-		if (null != carr)
-			dto = new CarriereDto(carr);
+			if (null != carr)
+				dto = new CarriereDto(carr);
+		} catch (Exception e) {
+			// le lien n'a pas été fait avec le grade
+			// #11956
+			SpcarrWithoutSpgradn carrWithoutGrade = spcarrRepository.getCarriereActiveWithoutGrad(noMatr);
+
+			if (null != carrWithoutGrade)
+				dto = new CarriereDto(carrWithoutGrade);
+		}
 
 		return dto;
 	}
