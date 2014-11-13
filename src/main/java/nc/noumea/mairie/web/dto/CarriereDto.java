a2e8c07a515515c4c222bb929204490103bc196b
nicno85 <nicno85@ville-noumea.nc>
2014-11-13 04:30:04 +0000

    #11956 : EAE --> erreur sur affectation active pour certains agents

--- src/main/java/nc/noumea/mairie/web/dto/CarriereDto.java
+++ src/main/java/nc/noumea/mairie/web/dto/CarriereDto.java
@@ -5,20 +5,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import nc.noumea.mairie.model.bean.Spcarr;
+import nc.noumea.mairie.model.bean.SpcarrWithoutSpgradn;
 
 public class CarriereDto {
 
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-	
+
 	private Date dateDebut;
 	private Integer noMatr;
 	private Integer codeCategorie;
 	private String libelleCategorie;
 	private GradeDto grade;
 
-	public CarriereDto(){
+	public CarriereDto() {
 	}
-			
+
 	public CarriereDto(Spcarr carr) {
 		try {
 			this.dateDebut = sdf.parse(carr.getId().getDatdeb().toString());
@@ -26,16 +27,29 @@ public class CarriereDto {
 			this.dateDebut = null;
 		}
 		this.noMatr = carr.getId().getNomatr();
-		if(null != carr.getCategorie()) {
+		if (null != carr.getCategorie()) {
 			this.codeCategorie = carr.getCategorie().getCodeCategorie();
 			this.libelleCategorie = carr.getCategorie().getLibelleCategorie();
 		}
-		
-		if(null != carr.getGrade()){
+
+		if (null != carr.getGrade()) {
 			grade = new GradeDto(carr.getGrade());
 		}
 	}
-	
+
+	public CarriereDto(SpcarrWithoutSpgradn carrWithoutGrade) {
+		try {
+			this.dateDebut = sdf.parse(carrWithoutGrade.getId().getDatdeb().toString());
+		} catch (ParseException e) {
+			this.dateDebut = null;
+		}
+		this.noMatr = carrWithoutGrade.getId().getNomatr();
+		if (null != carrWithoutGrade.getCategorie()) {
+			this.codeCategorie = carrWithoutGrade.getCategorie().getCodeCategorie();
+			this.libelleCategorie = carrWithoutGrade.getCategorie().getLibelleCategorie();
+		}
+	}
+
 	public Date getDateDebut() {
 		return dateDebut;
 	}
@@ -75,5 +89,5 @@ public class CarriereDto {
 	public void setGrade(GradeDto grade) {
 		this.grade = grade;
 	}
-	
+
 }
