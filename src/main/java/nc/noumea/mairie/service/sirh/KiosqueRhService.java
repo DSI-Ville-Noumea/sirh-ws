55d201878a3e28953bbb694299e7d9bd2e4a7cc3
nicno85 <nicno85@ville-noumea.nc>
2014-11-17 04:26:38 +0000

    #12074 : KIOSQUE --> blabla accueil

--- src/main/java/nc/noumea/mairie/service/sirh/KiosqueRhService.java
+++ src/main/java/nc/noumea/mairie/service/sirh/KiosqueRhService.java
@@ -6,12 +6,13 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import nc.noumea.mairie.model.bean.sirh.AccueilRh;
 import nc.noumea.mairie.model.bean.sirh.ReferentRh;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReferentService implements IReferentRhService {
+public class KiosqueRhService implements IKiosqueRhService {
 
 	@PersistenceContext(unitName = "sirhPersistenceUnit")
 	transient EntityManager sirhEntityManager;
@@ -21,6 +22,14 @@ public class ReferentService implements IReferentRhService {
 		TypedQuery<ReferentRh> query = sirhEntityManager.createQuery("select ref from ReferentRh ref ",
 				ReferentRh.class);
 		List<ReferentRh> lc = query.getResultList();
+
+		return lc;
+	}
+
+	@Override
+	public List<AccueilRh> getListeAccueilRh() {
+		TypedQuery<AccueilRh> query = sirhEntityManager.createQuery("select ref from AccueilRh ref ", AccueilRh.class);
+		List<AccueilRh> lc = query.getResultList();
 
 		return lc;
 	}
--- src/test/java/nc/noumea/mairie/service/sirh/KiosqueRhServiceTest.java
+++ src/test/java/nc/noumea/mairie/service/sirh/KiosqueRhServiceTest.java
@@ -9,13 +9,14 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import nc.noumea.mairie.model.bean.sirh.AccueilRh;
 import nc.noumea.mairie.model.bean.sirh.ReferentRh;
 
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.test.util.ReflectionTestUtils;
 
-public class ReferentServiceTest {
+public class KiosqueRhServiceTest {
 
 	@SuppressWarnings("unchecked")
 	@Test
@@ -26,7 +27,7 @@ public class ReferentServiceTest {
 		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
 		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(ReferentRh.class))).thenReturn(mockQuery);
 
-		ReferentService service = new ReferentService();
+		KiosqueRhService service = new KiosqueRhService();
 		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
 
 		// When
@@ -52,7 +53,7 @@ public class ReferentServiceTest {
 		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
 		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(ReferentRh.class))).thenReturn(mockQuery);
 
-		ReferentService service = new ReferentService();
+		KiosqueRhService service = new KiosqueRhService();
 		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
 
 		// When
@@ -62,5 +63,52 @@ public class ReferentServiceTest {
 		assertEquals(2, result.size());
 		assertEquals(new Integer(9005138), result.get(0).getIdAgentReferent());
 		assertEquals(new Integer(9005139), result.get(1).getIdAgentReferent());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getListeAccueilRh_returnNull() {
+		TypedQuery<AccueilRh> mockQuery = Mockito.mock(TypedQuery.class);
+		Mockito.when(mockQuery.getResultList()).thenReturn(new ArrayList<AccueilRh>());
+
+		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
+		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(AccueilRh.class))).thenReturn(mockQuery);
+
+		KiosqueRhService service = new KiosqueRhService();
+		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
+
+		// When
+		List<AccueilRh> result = service.getListeAccueilRh();
+
+		// Then
+
+		assertEquals(0, result.size());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getListeAccueilRh_returnListOf2AccueilRh() {
+		// Given
+		AccueilRh ref1 = new AccueilRh();
+		ref1.setTexteAccueilKiosque("texte 1");
+		AccueilRh ref2 = new AccueilRh();
+		ref2.setTexteAccueilKiosque("texte 2");
+
+		TypedQuery<AccueilRh> mockQuery = Mockito.mock(TypedQuery.class);
+		Mockito.when(mockQuery.getResultList()).thenReturn(Arrays.asList(ref1, ref2));
+
+		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
+		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(AccueilRh.class))).thenReturn(mockQuery);
+
+		KiosqueRhService service = new KiosqueRhService();
+		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
+
+		// When
+		List<AccueilRh> result = service.getListeAccueilRh();
+
+		// Then
+		assertEquals(2, result.size());
+		assertEquals("texte 1", result.get(0).getTexteAccueilKiosque());
+		assertEquals("texte 2", result.get(1).getTexteAccueilKiosque());
 	}
 }
