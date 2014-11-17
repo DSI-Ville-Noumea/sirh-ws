678638c61349c990605ccd0e73483e4c10dea969
nicno85 <nicno85@ville-noumea.nc>
2014-11-17 04:26:38 +0000

    #12074 : KIOSQUE --> blabla accueil

--- src/main/java/nc/noumea/mairie/web/controller/KiosqueRHController.java
+++ src/main/java/nc/noumea/mairie/web/controller/KiosqueRHController.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import nc.noumea.mairie.model.bean.sirh.AccueilRh;
 import nc.noumea.mairie.model.bean.sirh.ReferentRh;
 import nc.noumea.mairie.service.ISiservService;
 import nc.noumea.mairie.service.sirh.IAgentService;
-import nc.noumea.mairie.service.sirh.IReferentRhService;
+import nc.noumea.mairie.service.sirh.IKiosqueRhService;
 import nc.noumea.mairie.tools.transformer.MSDateTransformer;
+import nc.noumea.mairie.web.dto.AccueilRhDto;
 import nc.noumea.mairie.web.dto.ReferentRhDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,11 +26,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import flexjson.JSONSerializer;
 
 @Controller
-@RequestMapping("/referentRH")
-public class ReferentRHController {
+@RequestMapping("/kiosqueRH")
+public class KiosqueRHController {
 
 	@Autowired
-	private IReferentRhService refSrv;
+	private IKiosqueRhService kiosqueSrv;
 
 	@Autowired
 	private IAgentService agentSrv;
@@ -41,11 +43,29 @@ public class ReferentRHController {
 	@Transactional(readOnly = true)
 	public ResponseEntity<String> getListeReferentRH() throws ParseException {
 
-		List<ReferentRh> lc = refSrv.getListeReferentRH();
+		List<ReferentRh> lc = kiosqueSrv.getListeReferentRH();
 		List<ReferentRhDto> listeRef = new ArrayList<ReferentRhDto>();
 		for (ReferentRh c : lc) {
 			ReferentRhDto dto = new ReferentRhDto(c, agentSrv.getAgent(c.getIdAgentReferent()), siservSrv.getService(c
 					.getServi()));
+			listeRef.add(dto);
+		}
+
+		String response = new JSONSerializer().exclude("*.class").transform(new MSDateTransformer(), Date.class)
+				.deepSerialize(listeRef);
+
+		return new ResponseEntity<String>(response, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/getListeAccueilRH", produces = "application/json;charset=utf-8", method = RequestMethod.GET)
+	@ResponseBody
+	@Transactional(readOnly = true)
+	public ResponseEntity<String> getListeAccueilRH() throws ParseException {
+
+		List<AccueilRh> lc = kiosqueSrv.getListeAccueilRh();
+		List<AccueilRhDto> listeRef = new ArrayList<AccueilRhDto>();
+		for (AccueilRh c : lc) {
+			AccueilRhDto dto = new AccueilRhDto(c);
 			listeRef.add(dto);
 		}
 
