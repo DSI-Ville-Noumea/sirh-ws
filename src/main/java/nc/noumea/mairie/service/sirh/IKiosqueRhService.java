bbfed831c8afa4c1c480937fbf5c6ae9916cb286
nicno85 <nicno85@ville-noumea.nc>
2014-11-17 04:26:38 +0000

    #12074 : KIOSQUE --> blabla accueil

--- src/main/java/nc/noumea/mairie/service/sirh/IKiosqueRhService.java
+++ src/main/java/nc/noumea/mairie/service/sirh/IKiosqueRhService.java
@@ -2,10 +2,13 @@ package nc.noumea.mairie.service.sirh;
 
 import java.util.List;
 
+import nc.noumea.mairie.model.bean.sirh.AccueilRh;
 import nc.noumea.mairie.model.bean.sirh.ReferentRh;
 
-public interface IReferentRhService {
+public interface IKiosqueRhService {
 
 	List<ReferentRh> getListeReferentRH();
+
+	List<AccueilRh> getListeAccueilRh();
 
 }
