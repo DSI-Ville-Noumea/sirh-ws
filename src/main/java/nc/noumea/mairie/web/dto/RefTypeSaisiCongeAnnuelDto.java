deb09c3b54a25d2ddfe87606f67ffa69035379e0
nicno85 <nicno85@ville-noumea.nc>
2014-12-01 02:26:01 +0000

    #12289 : WS utiles à la saisie /modif d'une demande de congé annuel

---
+++ src/main/java/nc/noumea/mairie/web/dto/RefTypeSaisiCongeAnnuelDto.java
@@ -0,0 +1,117 @@
+package nc.noumea.mairie.web.dto;
+
+public class RefTypeSaisiCongeAnnuelDto {
+
+	private Integer idRefTypeSaisiCongeAnnuel;
+	private String codeBaseHoraireAbsence;
+	private String description;
+	private Integer idRefTypeDemande;
+	private boolean calendarDateDebut;
+	private boolean chkDateDebut;
+	private boolean calendarDateFin;
+	private boolean chkDateFin;
+	private boolean calendarDateReprise;
+	private Integer quotaMultiple;
+	private boolean decompteSamedi;
+	private boolean consecutif;
+
+	public RefTypeSaisiCongeAnnuelDto() {
+	}
+
+	public Integer getIdRefTypeSaisiCongeAnnuel() {
+		return idRefTypeSaisiCongeAnnuel;
+	}
+
+	public void setIdRefTypeSaisiCongeAnnuel(Integer idRefTypeSaisiCongeAnnuel) {
+		this.idRefTypeSaisiCongeAnnuel = idRefTypeSaisiCongeAnnuel;
+	}
+
+	public String getCodeBaseHoraireAbsence() {
+		return codeBaseHoraireAbsence;
+	}
+
+	public void setCodeBaseHoraireAbsence(String codeBaseHoraireAbsence) {
+		this.codeBaseHoraireAbsence = codeBaseHoraireAbsence;
+	}
+
+	public Integer getIdRefTypeDemande() {
+		return idRefTypeDemande;
+	}
+
+	public void setIdRefTypeDemande(Integer idRefTypeDemande) {
+		this.idRefTypeDemande = idRefTypeDemande;
+	}
+
+	public boolean isCalendarDateDebut() {
+		return calendarDateDebut;
+	}
+
+	public void setCalendarDateDebut(boolean calendarDateDebut) {
+		this.calendarDateDebut = calendarDateDebut;
+	}
+
+	public boolean isChkDateDebut() {
+		return chkDateDebut;
+	}
+
+	public void setChkDateDebut(boolean chkDateDebut) {
+		this.chkDateDebut = chkDateDebut;
+	}
+
+	public boolean isCalendarDateFin() {
+		return calendarDateFin;
+	}
+
+	public void setCalendarDateFin(boolean calendarDateFin) {
+		this.calendarDateFin = calendarDateFin;
+	}
+
+	public boolean isChkDateFin() {
+		return chkDateFin;
+	}
+
+	public void setChkDateFin(boolean chkDateFin) {
+		this.chkDateFin = chkDateFin;
+	}
+
+	public boolean isCalendarDateReprise() {
+		return calendarDateReprise;
+	}
+
+	public void setCalendarDateReprise(boolean calendarDateReprise) {
+		this.calendarDateReprise = calendarDateReprise;
+	}
+
+	public Integer getQuotaMultiple() {
+		return quotaMultiple;
+	}
+
+	public void setQuotaMultiple(Integer quotaMultiple) {
+		this.quotaMultiple = quotaMultiple;
+	}
+
+	public boolean isDecompteSamedi() {
+		return decompteSamedi;
+	}
+
+	public void setDecompteSamedi(boolean decompteSamedi) {
+		this.decompteSamedi = decompteSamedi;
+	}
+
+	public boolean isConsecutif() {
+		return consecutif;
+	}
+
+	public void setConsecutif(boolean consecutif) {
+		this.consecutif = consecutif;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
