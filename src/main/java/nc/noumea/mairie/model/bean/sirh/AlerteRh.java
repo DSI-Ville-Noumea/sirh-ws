5a7dbebff5b296146058b7b1c8b8ded59c97164a
nicno85 <nicno85@ville-noumea.nc>
2015-06-09 02:55:17 +0000

    #15956 : WS utiles à : SIRH - parametres - kiosque : composant message alerte

---
+++ src/main/java/nc/noumea/mairie/model/bean/sirh/AlerteRh.java
@@ -0,0 +1,128 @@
+package nc.noumea.mairie.model.bean.sirh;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "P_ALERTE_KIOSQUE")
+@PersistenceUnit(unitName = "sirhPersistenceUnit")
+public class AlerteRh {
+
+	@Id
+	@Column(name = "ID_ALERTE_KIOSQUE")
+	private Integer idAlerteKiosque;
+
+	@Column(name = "TEXTE_ALERTE_KIOSQUE")
+	@Lob
+	private String texteAlerteKiosque;
+
+	@Column(name = "AGENT")
+	private boolean agent;
+
+	@Column(name = "APPRO_ABS")
+	private boolean approbateurABS;
+
+	@Column(name = "APPRO_PTG")
+	private boolean approbateurPTG;
+
+	@Column(name = "OPE_ABS")
+	private boolean operateurABS;
+
+	@Column(name = "OPE_PTG")
+	private boolean operateurPTG;
+
+	@Column(name = "VISEUR_ABS")
+	private boolean viseurABS;
+
+	@Column(name = "DATE_DEBUT")
+	private Date dateDebut;
+
+	@Column(name = "DATE_FIN")
+	private Date dateFin;
+
+	public Integer getIdAlerteKiosque() {
+		return idAlerteKiosque;
+	}
+
+	public void setIdAlerteKiosque(Integer idAlerteKiosque) {
+		this.idAlerteKiosque = idAlerteKiosque;
+	}
+
+	public String getTexteAlerteKiosque() {
+		return texteAlerteKiosque;
+	}
+
+	public void setTexteAlerteKiosque(String texteAlerteKiosque) {
+		this.texteAlerteKiosque = texteAlerteKiosque;
+	}
+
+	public boolean isAgent() {
+		return agent;
+	}
+
+	public void setAgent(boolean agent) {
+		this.agent = agent;
+	}
+
+	public boolean isApprobateurABS() {
+		return approbateurABS;
+	}
+
+	public void setApprobateurABS(boolean approbateurABS) {
+		this.approbateurABS = approbateurABS;
+	}
+
+	public boolean isApprobateurPTG() {
+		return approbateurPTG;
+	}
+
+	public void setApprobateurPTG(boolean approbateurPTG) {
+		this.approbateurPTG = approbateurPTG;
+	}
+
+	public boolean isOperateurABS() {
+		return operateurABS;
+	}
+
+	public void setOperateurABS(boolean operateurABS) {
+		this.operateurABS = operateurABS;
+	}
+
+	public boolean isOperateurPTG() {
+		return operateurPTG;
+	}
+
+	public void setOperateurPTG(boolean operateurPTG) {
+		this.operateurPTG = operateurPTG;
+	}
+
+	public boolean isViseurABS() {
+		return viseurABS;
+	}
+
+	public void setViseurABS(boolean viseurABS) {
+		this.viseurABS = viseurABS;
+	}
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+}
--- src/test/java/nc/noumea/mairie/service/sirh/KiosqueRhServiceTest.java
+++ src/test/java/nc/noumea/mairie/service/sirh/KiosqueRhServiceTest.java
@@ -8,16 +8,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import nc.noumea.mairie.model.bean.sirh.AccueilRh;
+import nc.noumea.mairie.model.bean.sirh.AlerteRh;
 import nc.noumea.mairie.model.bean.sirh.ReferentRh;
+import nc.noumea.mairie.ws.ISirhAbsWSConsumer;
+import nc.noumea.mairie.ws.ISirhPtgWSConsumer;
+import nc.noumea.mairie.ws.dto.ReturnMessageDto;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/META-INF/spring/applicationContext-test.xml" })
 public class KiosqueRhServiceTest {
+
+	@PersistenceContext(unitName = "sirhPersistenceUnit")
+	private EntityManager sirhPersistenceUnit;
+
+	@Autowired
+	IKiosqueRhService repository;
 
 	@SuppressWarnings("unchecked")
 	@Test
@@ -137,5 +156,123 @@ public class KiosqueRhServiceTest {
 		assertEquals(2, result.size());
 		assertEquals("texte 1", result.get(0).getTexteAccueilKiosque());
 		assertEquals("texte 2", result.get(1).getTexteAccueilKiosque());
+	}
+
+	@Test
+	@Transactional("sirhTransactionManager")
+	public void getListeAlerte_returnListOf2AlerteRh() {
+		// Given
+		AlerteRh ref1 = new AlerteRh();
+		ref1.setIdAlerteKiosque(1);
+		ref1.setTexteAlerteKiosque("texte 1");
+		ref1.setAgent(true);
+		ref1.setApprobateurABS(true);
+		ref1.setOperateurABS(true);
+		ref1.setDateDebut(new DateTime(2014, 2, 2, 0, 0, 0).toDate());
+		ref1.setDateFin(new DateTime(2016, 2, 2, 0, 0, 0).toDate());
+		sirhPersistenceUnit.persist(ref1);
+		AlerteRh ref2 = new AlerteRh();
+		ref2.setIdAlerteKiosque(2);
+		ref2.setTexteAlerteKiosque("texte 2");
+		ref2.setAgent(true);
+		ref2.setDateDebut(new DateTime(2014, 2, 2, 0, 0, 0).toDate());
+		ref2.setDateFin(new DateTime(2016, 2, 2, 0, 0, 0).toDate());
+		sirhPersistenceUnit.persist(ref2);
+		AlerteRh ref3 = new AlerteRh();
+		ref3.setIdAlerteKiosque(3);
+		ref3.setTexteAlerteKiosque("texte 3");
+		ref3.setDateDebut(new DateTime(2014, 2, 2, 0, 0, 0).toDate());
+		ref3.setDateFin(new DateTime(2016, 2, 2, 0, 0, 0).toDate());
+		sirhPersistenceUnit.persist(ref3);
+
+		// When
+		List<AlerteRh> result = repository.getListeAlerte(true, false, true, false, false);
+
+		// Then
+		assertEquals(2, result.size());
+		assertEquals("texte 1", result.get(0).getTexteAlerteKiosque());
+		assertEquals("texte 2", result.get(1).getTexteAlerteKiosque());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getAlerteRHByAgent_returnNull() {
+		Integer idAgent = 9005138;
+
+		ISirhAbsWSConsumer sirhAbsWSConsumer = Mockito.mock(ISirhAbsWSConsumer.class);
+		Mockito.when(sirhAbsWSConsumer.isUserApprobateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhAbsWSConsumer.isUserOperateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhAbsWSConsumer.isUserViseur(idAgent)).thenReturn(false);
+
+		ISirhPtgWSConsumer sirhPtgWSConsumer = Mockito.mock(ISirhPtgWSConsumer.class);
+		Mockito.when(sirhPtgWSConsumer.isUserApprobateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhPtgWSConsumer.isUserOperateur(idAgent)).thenReturn(false);
+
+		TypedQuery<AlerteRh> mockQuery = Mockito.mock(TypedQuery.class);
+		Mockito.when(mockQuery.getResultList()).thenReturn(new ArrayList<AlerteRh>());
+
+		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
+		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(AlerteRh.class))).thenReturn(mockQuery);
+
+		KiosqueRhService service = new KiosqueRhService();
+		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
+		ReflectionTestUtils.setField(service, "sirhAbsWSConsumer", sirhAbsWSConsumer);
+		ReflectionTestUtils.setField(service, "sirhPtgWSConsumer", sirhPtgWSConsumer);
+
+		// When
+		ReturnMessageDto result = service.getAlerteRHByAgent(idAgent);
+
+		// Then
+
+		assertEquals(0, result.getInfos().size());
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void getAlerteRHByAgent_returnListOf2AlerteRh() {
+		Integer idAgent = 9005138;
+
+		AlerteRh ref1 = new AlerteRh();
+		ref1.setIdAlerteKiosque(1);
+		ref1.setTexteAlerteKiosque("texte 1");
+		ref1.setAgent(true);
+		ref1.setApprobateurABS(true);
+		ref1.setOperateurABS(true);
+		ref1.setDateDebut(new DateTime(2014, 2, 2, 0, 0, 0).toDate());
+		ref1.setDateFin(new DateTime(2016, 2, 2, 0, 0, 0).toDate());
+		AlerteRh ref2 = new AlerteRh();
+		ref2.setIdAlerteKiosque(2);
+		ref2.setTexteAlerteKiosque("texte 2");
+		ref2.setAgent(true);
+		ref2.setDateDebut(new DateTime(2014, 2, 2, 0, 0, 0).toDate());
+		ref2.setDateFin(new DateTime(2016, 2, 2, 0, 0, 0).toDate());
+
+		ISirhAbsWSConsumer sirhAbsWSConsumer = Mockito.mock(ISirhAbsWSConsumer.class);
+		Mockito.when(sirhAbsWSConsumer.isUserApprobateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhAbsWSConsumer.isUserOperateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhAbsWSConsumer.isUserViseur(idAgent)).thenReturn(false);
+
+		ISirhPtgWSConsumer sirhPtgWSConsumer = Mockito.mock(ISirhPtgWSConsumer.class);
+		Mockito.when(sirhPtgWSConsumer.isUserApprobateur(idAgent)).thenReturn(false);
+		Mockito.when(sirhPtgWSConsumer.isUserOperateur(idAgent)).thenReturn(false);
+
+		TypedQuery<AlerteRh> mockQuery = Mockito.mock(TypedQuery.class);
+		Mockito.when(mockQuery.getResultList()).thenReturn(Arrays.asList(ref1, ref2));
+
+		EntityManager sirhEMMock = Mockito.mock(EntityManager.class);
+		Mockito.when(sirhEMMock.createQuery(Mockito.anyString(), Mockito.eq(AlerteRh.class))).thenReturn(mockQuery);
+
+		KiosqueRhService service = new KiosqueRhService();
+		ReflectionTestUtils.setField(service, "sirhEntityManager", sirhEMMock);
+		ReflectionTestUtils.setField(service, "sirhAbsWSConsumer", sirhAbsWSConsumer);
+		ReflectionTestUtils.setField(service, "sirhPtgWSConsumer", sirhPtgWSConsumer);
+
+		// When
+		ReturnMessageDto result = service.getAlerteRHByAgent(idAgent);
+
+		// Then
+		assertEquals(2, result.getInfos().size());
+		assertEquals("texte 1", result.getInfos().get(0));
+		assertEquals("texte 2", result.getInfos().get(1));
 	}
 }
