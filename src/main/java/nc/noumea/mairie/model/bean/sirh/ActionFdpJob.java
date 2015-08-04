91d806d6f12cadd8c05aa84129b2e1e0d4638191
nicno85 <nicno85@ville-noumea.nc>
2015-08-04 00:32:13 +0000

    #17444 : JOB : Dupliquer une entité unique dans l'arbre

--- src/main/java/nc/noumea/mairie/model/bean/sirh/ActionFdpJob.java
+++ src/main/java/nc/noumea/mairie/model/bean/sirh/ActionFdpJob.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.Table;
@@ -22,6 +24,7 @@ public class ActionFdpJob {
 
 	@Id
 	@Column(name = "ID_ACTION_FDP_JOB")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idActionFdpJob;
 
 	@Column(name = "ID_AGENT")
