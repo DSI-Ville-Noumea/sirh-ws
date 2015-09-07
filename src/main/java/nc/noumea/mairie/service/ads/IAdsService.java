3f62f01a0b90bb9ec5d1011739a1270aff3d0d7c
nicno85 <nicno85@ville-noumea.nc>
2015-09-07 00:03:39 +0000

    #18300 : SIRH - Merge sirh-ws

---
+++ src/main/java/nc/noumea/mairie/service/ads/IAdsService.java
@@ -0,0 +1,17 @@
+package nc.noumea.mairie.service.ads;
+
+import java.util.List;
+
+import nc.noumea.mairie.web.dto.EntiteDto;
+
+public interface IAdsService {
+
+	EntiteDto getEntiteByIdEntiteOptimise(Integer idEntite, List<EntiteDto> listEntiteDto);
+
+	String getSigleEntityInEntiteDtoTreeByIdEntite(EntiteDto entite,
+			Integer idServiceAds);
+
+	EntiteDto getInfoSiservByIdEntite(Integer idEntite);
+
+	EntiteDto getEntiteByIdEntiteOptimiseWithWholeTree(Integer idEntite, EntiteDto root);
+}
