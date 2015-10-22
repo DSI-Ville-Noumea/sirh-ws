2ff2035b9f8d7e83f28fe1b83dc40c8efa5d4d7a
rebjo84 <rebjo84@ville-noumea.nc>
2015-10-22 23:47:13 +0000

    #19257 TR : WS : retourne la dernière affectations de plusieurs agent avec base congé et primes

--- src/main/java/nc/noumea/mairie/service/sirh/IAffectationService.java
+++ src/main/java/nc/noumea/mairie/service/sirh/IAffectationService.java
@@ -1,8 +1,10 @@
 package nc.noumea.mairie.service.sirh;
 
+import java.util.Date;
 import java.util.List;
 
 import nc.noumea.mairie.model.bean.sirh.Affectation;
+import nc.noumea.mairie.web.dto.AffectationDto;
 
 public interface IAffectationService {
 
@@ -13,4 +15,7 @@ public interface IAffectationService {
 	public Affectation getAffectationByIdFichePoste(Integer idFichePoste);
 
 	public List<Affectation> getListAffectationActiveByIdFichePoste(Integer idFichePoste);
+
+	List<AffectationDto> getListeAffectationDtosForListAgentByPeriode(
+			List<Integer> listIdsAgent, Date dateDebut, Date dateFin);
 }
--- src/main/java/nc/noumea/mairie/web/controller/PointageController.java
+++ src/main/java/nc/noumea/mairie/web/controller/PointageController.java
@@ -1,9 +1,13 @@
 package nc.noumea.mairie.web.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import nc.noumea.mairie.service.sirh.HelperService;
+import nc.noumea.mairie.service.sirh.IAffectationService;
 import nc.noumea.mairie.service.sirh.IPointageService;
+import nc.noumea.mairie.web.dto.AffectationDto;
 import nc.noumea.mairie.web.dto.BaseHorairePointageDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +16,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import flexjson.JSONDeserializer;
 import flexjson.JSONSerializer;
 
 @Controller
@@ -25,6 +31,12 @@ public class PointageController {
 
 	@Autowired
 	private IPointageService pointageSrv;
+	
+	@Autowired
+	private IAffectationService affectationService;
+	
+	@Autowired
+	private HelperService helperService;
 
 	@ResponseBody
 	@RequestMapping(value = "/listPrimePointages", method = RequestMethod.GET)
@@ -67,6 +79,49 @@ public class PointageController {
 			@RequestParam(value = "dateFin", required = true) @DateTimeFormat(pattern = "yyyyMMdd") Date dateFin) {
 
 		List<Integer> result = pointageSrv.getListAgentsWithPrimeTIDOnAffectation(dateDebut, dateFin);
+
+		if (result.size() == 0)
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+
+		String json = new JSONSerializer().exclude("*.class").serialize(result);
+
+		return new ResponseEntity<String>(json, HttpStatus.OK);
+	}
+	
+	/**
+	 * Retourne une liste d'affectation avec la base congé et les primes pointages sur l'affectation 
+	 * pour une liste d agents donne en parametre
+	 * et entre deux dates
+	 * 
+	 * @param dateDebut Date yyyyMMdd
+	 * @param dateFin Date yyyyMMdd
+	 * @param agentsApprouvesJson List<Integer> Liste d ID agent
+	 * @return List<AffectationDto>
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/listeAffectationDtosForListAgentByPeriode", method = RequestMethod.GET)
+	@Transactional(readOnly = true)
+	public ResponseEntity<String> getListeAffectationDtosForListAgentByPeriode(
+			@RequestParam(value = "dateDebut", required = true) @DateTimeFormat(pattern = "yyyyMMdd") Date dateDebut,
+			@RequestParam(value = "dateFin", required = true) @DateTimeFormat(pattern = "yyyyMMdd") Date dateFin,
+			@RequestBody String agentsApprouvesJson) {
+		
+		List<Integer> listIdsAgent = new JSONDeserializer<List<Integer>>()
+				.use(null, ArrayList.class)
+				.use("values", Integer.class)
+				.deserialize(agentsApprouvesJson);
+
+		if (null == listIdsAgent || listIdsAgent.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
+		}
+
+		// on remanie l'idAgent
+		List<Integer> listIdsAgentRemanie = new ArrayList<Integer>();
+		for (Integer idAgent : listIdsAgent) {
+			listIdsAgentRemanie.add(helperService.remanieIdAgent(idAgent));
+		}
+			
+		List<AffectationDto> result = affectationService.getListeAffectationDtosForListAgentByPeriode(listIdsAgent, dateDebut, dateFin);
 
 		if (result.size() == 0)
 			return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
