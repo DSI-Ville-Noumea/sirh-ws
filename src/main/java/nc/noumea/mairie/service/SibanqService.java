d7d12197d213c8a9bbadbb973a53a79053687c3f
nicno85 <nicno85@ville-noumea.nc>
2015-12-17 03:31:35 +0000

    #20522  : WS banque/guichet pour CP

--- src/main/java/nc/noumea/mairie/service/SibanqService.java
+++ src/main/java/nc/noumea/mairie/service/SibanqService.java
@@ -1,12 +1,15 @@
 package nc.noumea.mairie.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import nc.noumea.mairie.model.bean.SiBanqGuichet;
 import nc.noumea.mairie.model.bean.Sibanq;
+import nc.noumea.mairie.web.dto.BanqueGuichetDto;
 
 import org.springframework.stereotype.Service;
 
@@ -21,8 +24,7 @@ public class SibanqService implements ISibanqService {
 
 		Sibanq res = null;
 
-		TypedQuery<Sibanq> query = sirhEntityManager.createQuery(
-				"select sb from Sibanq sb where sb.idBanque = :idBanque", Sibanq.class);
+		TypedQuery<Sibanq> query = sirhEntityManager.createQuery("select sb from Sibanq sb where sb.idBanque = :idBanque", Sibanq.class);
 
 		query.setParameter("idBanque", codeBanque);
 		List<Sibanq> lfp = query.getResultList();
@@ -31,5 +33,21 @@ public class SibanqService implements ISibanqService {
 			res = fp;
 
 		return res;
+	}
+
+	@Override
+	public List<BanqueGuichetDto> getListBanqueAvecGuichet() {
+		List<BanqueGuichetDto> result = new ArrayList<BanqueGuichetDto>();
+
+		// on recupere toutes les banques/guichet présent en base
+		TypedQuery<SiBanqGuichet> query = sirhEntityManager.createQuery("select sb from SiBanqGuichet sb", SiBanqGuichet.class);
+		List<SiBanqGuichet> lfp = query.getResultList();
+
+		for (SiBanqGuichet bq : lfp) {
+			BanqueGuichetDto dto = new BanqueGuichetDto(bq);
+			result.add(dto);
+		}
+		return result;
+
 	}
 }
