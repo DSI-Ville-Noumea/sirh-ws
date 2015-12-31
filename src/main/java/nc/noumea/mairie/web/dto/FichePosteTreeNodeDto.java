479de3bf9fbbc17cd72f2a37094b6df496472cf4
nicno85 <nicno85@ville-noumea.nc>
2015-12-31 02:43:22 +0000

    #20792 : ORG : deplacer la section affichée entre ( )

--- src/main/java/nc/noumea/mairie/web/dto/FichePosteTreeNodeDto.java
+++ src/main/java/nc/noumea/mairie/web/dto/FichePosteTreeNodeDto.java
@@ -12,57 +12,64 @@ public class FichePosteTreeNodeDto extends FichePosteDto {
 	private Integer idAgent;
 	private List<FichePosteTreeNodeDto> fichePostesEnfant;
 	private FichePosteTreeNodeDto fichePosteParent;
-	
+
 	public FichePosteTreeNodeDto() {
 		fichePostesEnfant = new ArrayList<FichePosteTreeNodeDto>();
 	}
-	
+
 	public FichePosteTreeNodeDto(Integer _idFichePoste, Integer _idFichePosteParent, Integer _idAgent) {
 		this();
 		idFichePoste = _idFichePoste;
 		idFichePosteParent = _idFichePosteParent;
 		idAgent = _idAgent;
 	}
-	
-	public FichePosteTreeNodeDto(
-			Integer _idFichePoste, Integer _idFichePosteParent, Integer _idAgent, 
-			FichePoste fichePoste, String sigle) {
-		
-		super(fichePoste, sigle);
+
+	public FichePosteTreeNodeDto(Integer _idFichePoste, Integer _idFichePosteParent, Integer _idAgent, FichePoste fichePoste, String sigle, String libelleService) {
+
+		super(fichePoste, sigle, libelleService);
 		idFichePoste = _idFichePoste;
 		idFichePosteParent = _idFichePosteParent;
 		idAgent = _idAgent;
 
 		fichePostesEnfant = new ArrayList<FichePosteTreeNodeDto>();
 	}
-	
+
 	public Integer getIdFichePoste() {
 		return idFichePoste;
 	}
+
 	public void setIdFichePoste(Integer idFichePoste) {
 		this.idFichePoste = idFichePoste;
 	}
+
 	public Integer getIdFichePosteParent() {
 		return idFichePosteParent;
 	}
+
 	public void setIdFichePosteParent(Integer idFichePosteParent) {
 		this.idFichePosteParent = idFichePosteParent;
 	}
+
 	public Integer getIdAgent() {
 		return idAgent;
 	}
+
 	public void setIdAgent(Integer idAgent) {
 		this.idAgent = idAgent;
 	}
+
 	public List<FichePosteTreeNodeDto> getFichePostesEnfant() {
 		return fichePostesEnfant;
 	}
+
 	public void setFichePostesEnfant(List<FichePosteTreeNodeDto> fichePostesEnfant) {
 		this.fichePostesEnfant = fichePostesEnfant;
 	}
+
 	public FichePosteTreeNodeDto getFichePosteParent() {
 		return fichePosteParent;
 	}
+
 	public void setFichePosteParent(FichePosteTreeNodeDto fichePosteParent) {
 		this.fichePosteParent = fichePosteParent;
 	}
