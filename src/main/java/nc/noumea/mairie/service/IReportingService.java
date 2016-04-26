cca19ae9329718119557b0734707c74cf479193a
nicno85 <nicno85@ville-noumea.nc>
2016-04-26 23:52:18 +0000

    #30641 : Tableau CAP --> erreur

--- src/main/java/nc/noumea/mairie/service/IReportingService.java
+++ src/main/java/nc/noumea/mairie/service/IReportingService.java
@@ -1,13 +1,17 @@
 package nc.noumea.mairie.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import nc.noumea.mairie.web.dto.avancements.CommissionAvancementDto;
+
+import com.lowagie.text.DocumentException;
+
 public interface IReportingService {
 
 	byte[] getFichePosteReportAsByteArray(int idFichePoste) throws Exception;
 
-	byte[] getTableauAvancementsReportAsByteArray(int idCap, int idCadreEmploi, boolean avisEAE) throws Exception;
-
-	byte[] getArretesReportAsByteArray(String csvIdAgents, boolean isChangementClasse, int year, boolean isDetache)
-			throws Exception;
+	byte[] getArretesReportAsByteArray(String csvIdAgents, boolean isChangementClasse, int year, boolean isDetache) throws Exception;
 
 	byte[] getFichePosteSIRHReportAsByteArray(int idFichePoste) throws Exception;
 
@@ -18,4 +22,6 @@ public interface IReportingService {
 	byte[] getAccompagnementSIRHReportAsByteArray(String csvIdSuiviMedical, String typePopulation) throws Exception;
 
 	byte[] getContratSIRHReportAsByteArray(Integer idAgent, Integer idContrat) throws Exception;
+
+	byte[] getTableauAvancementPDF(CommissionAvancementDto dto) throws DocumentException, MalformedURLException, IOException;
 }
