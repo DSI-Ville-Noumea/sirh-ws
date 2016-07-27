503dfba614c6c2efd35fbbc2c28192ed5a9d993a
nicno85 <nicno85@ville-noumea.nc>
2016-07-27 00:36:27 +0000

    #32210 : Suivi médical - suppression modèles

--- src/main/java/nc/noumea/mairie/web/controller/SuiviMedicalController.java
+++ src/main/java/nc/noumea/mairie/web/controller/SuiviMedicalController.java
@@ -4,8 +4,18 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 import nc.noumea.mairie.model.bean.sirh.SuiviMedical;
-import nc.noumea.mairie.service.IReportingService;
 import nc.noumea.mairie.service.sirh.ISuiviMedicalService;
 import nc.noumea.mairie.web.dto.AccompagnementVMDto;
 import nc.noumea.mairie.web.dto.AgentWithServiceDto;
@@ -15,41 +25,27 @@ import nc.noumea.mairie.web.dto.ListVMDto;
 import nc.noumea.mairie.web.dto.MedecinDto;
 import nc.noumea.mairie.ws.IADSWSConsumer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
 @Controller
 @RequestMapping("/suiviMedical")
 public class SuiviMedicalController {
 
-	private Logger logger = LoggerFactory.getLogger(SuiviMedicalController.class);
+	private Logger					logger	= LoggerFactory.getLogger(SuiviMedicalController.class);
 
 	@Autowired
-	private ISuiviMedicalService smSrv;
+	private ISuiviMedicalService	smSrv;
 
 	@Autowired
-	private IReportingService reportingService;
-
-	@Autowired
-	private IADSWSConsumer adsConsumer;
+	private IADSWSConsumer			adsConsumer;
 
 	@ResponseBody
 	@RequestMapping(value = "/xml/getConvocationSIRH", produces = "application/xml", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ModelAndView getXmlConvocationSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical, @RequestParam("typePopulation") String typePopulation) throws ParseException {
+	public ModelAndView getXmlConvocationSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical,
+			@RequestParam("typePopulation") String typePopulation) throws ParseException {
 
-		logger.debug("entered GET [suiviMedical/xml/getConvocationSIRH] => getXmlConvocationSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} ", csvIdSuiviMedical, typePopulation);
+		logger.debug(
+				"entered GET [suiviMedical/xml/getConvocationSIRH] => getXmlConvocationSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} ",
+				csvIdSuiviMedical, typePopulation);
 
 		ListVMDto listDto = new ListVMDto();
 		listDto.setTypePopulation(typePopulation);
@@ -85,10 +81,12 @@ public class SuiviMedicalController {
 	@ResponseBody
 	@RequestMapping(value = "/xml/getLettreAccompagnementSIRH", produces = "application/xml", method = RequestMethod.GET)
 	@Transactional(readOnly = true)
-	public ModelAndView getXmlLettreAccompagnementSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical, @RequestParam("typePopulation") String typePopulation) throws ParseException {
+	public ModelAndView getXmlLettreAccompagnementSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical,
+			@RequestParam("typePopulation") String typePopulation) throws ParseException {
 
-		logger.debug("entered GET [suiviMedical/xml/getLettreAccompagnementSIRH] => getXmlLettreAccompagnementSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} ", csvIdSuiviMedical,
-				typePopulation);
+		logger.debug(
+				"entered GET [suiviMedical/xml/getLettreAccompagnementSIRH] => getXmlLettreAccompagnementSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} ",
+				csvIdSuiviMedical, typePopulation);
 
 		ListVMDto listDto = new ListVMDto();
 		listDto.setTypePopulation(typePopulation);
@@ -130,63 +128,6 @@ public class SuiviMedicalController {
 		}
 
 		return new ModelAndView("xmlView", "object", listDto);
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/downloadConvocationSIRH", method = RequestMethod.GET)
-	@Transactional(readOnly = true)
-	public ResponseEntity<byte[]> downloadConvocationSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical, @RequestParam("typePopulation") String typePopulation,
-			@RequestParam("mois") String mois, @RequestParam("annee") String annee) throws ParseException {
-
-		logger.debug("entered GET [suiviMedical/downloadConvocationSIRH] => downloadConvocationSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} and mois = {} and annee = {} ",
-				csvIdSuiviMedical, typePopulation, mois, annee);
-
-		if (csvIdSuiviMedical == null)
-			return new ResponseEntity<byte[]>(HttpStatus.NOT_FOUND);
-
-		byte[] responseData = null;
-
-		try {
-			responseData = reportingService.getConvocationSIRHReportAsByteArray(csvIdSuiviMedical, typePopulation);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			return new ResponseEntity<byte[]>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/doc");
-		headers.add("Content-Disposition", String.format("attachment; filename=\"SM_Convocation_%s_%s_%s.doc\"", typePopulation, mois, annee));
-
-		return new ResponseEntity<byte[]>(responseData, headers, HttpStatus.OK);
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/downloadLettreAccompagnementSIRH", method = RequestMethod.GET)
-	@Transactional(readOnly = true)
-	public ResponseEntity<byte[]> downloadLettreAccompagnementSIRH(@RequestParam("csvIdSuiviMedical") String csvIdSuiviMedical, @RequestParam("typePopulation") String typePopulation,
-			@RequestParam("mois") String mois, @RequestParam("annee") String annee) throws ParseException {
-
-		logger.debug(
-				"entered GET [suiviMedical/downloadLettreAccompagnementSIRH] => downloadLettreAccompagnementSIRH with parameter csvIdSuiviMedical = {} and typePopulation = {} and mois = {} and annee = {} ",
-				csvIdSuiviMedical, typePopulation, mois, annee);
-
-		if (csvIdSuiviMedical == null)
-			return new ResponseEntity<byte[]>(HttpStatus.NOT_FOUND);
-
-		byte[] responseData = null;
-
-		try {
-			responseData = reportingService.getAccompagnementSIRHReportAsByteArray(csvIdSuiviMedical, typePopulation);
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			return new ResponseEntity<byte[]>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/doc");
-		headers.add("Content-Disposition", String.format("attachment; filename=\"SM_Lettre_Accompagnement_%s_%s_%s.doc\"", typePopulation, mois, annee));
-
-		return new ResponseEntity<byte[]>(responseData, headers, HttpStatus.OK);
 	}
 
 }
