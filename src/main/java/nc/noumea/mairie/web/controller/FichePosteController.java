80525e3b64b0a16381821d1701d4792151d71dc8
Gael Wittorski <gael.wittorski@agilesoft.nc>
2017-08-30 15:08:17 +1100

    #40323 mime type and name

--- src/main/java/nc/noumea/mairie/web/controller/FichePosteController.java
+++ src/main/java/nc/noumea/mairie/web/controller/FichePosteController.java
@@ -240,8 +240,8 @@ public class FichePosteController {
 		}
 
 		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-Type", "application/doc");
-		headers.add("Content-Disposition", String.format("attachment; filename=\"FP_%s.doc\"", fp.getIdFichePoste()));
+		headers.add("Content-Type", "application/pdf");
+		headers.add("Content-Disposition", String.format("attachment; filename=\"FP_%s.pdf\"", fp.getIdFichePoste()));
 
 		return new ResponseEntity<byte[]>(responseData, headers, HttpStatus.OK);
 	}
